d3e146fd3dd7b142b102de9bb0665e90e45a3657
carlos2140485 <carlossibim210@gmail.com>
2020-05-29 18:53:31 -0300

    Criado método de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -6,6 +7,14 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     carlos = Pessoa(nome='Carlos')
@@ -21,3 +30,9 @@ if __name__ == '__main__':
     del luciano.filhos
     print(luciano.__dict__)
     print(carlos.__dict__)
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(carlos.olhos)
+    print(id(Pessoa.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributo_de_classe(),luciano.nome_e_atributo_de_classe())
