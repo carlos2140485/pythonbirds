3d3efd736b3aa58cf194d75aa457869ef3b5acc8
carlos2140485 <carlossibim210@gmail.com>
2020-06-04 15:42:30 -0300

    atualizado codigo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,7 +6,7 @@ class Pessoa:
         self.filhos = (filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -16,9 +16,17 @@ class Pessoa:
     def nome_e_atributo_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    carlos = Pessoa(nome='Carlos')
-    luciano = Pessoa(carlos, nome='Luciano')
+    carlos = Mutante(nome='Carlos')
+    luciano = Homem(carlos, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -36,3 +44,9 @@ if __name__ == '__main__':
     print(id(Pessoa.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributo_de_classe(),luciano.nome_e_atributo_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa,Homem))
+    print(carlos.olhos)
+    print(luciano.cumprimentar())
+    print(carlos.cumprimentar())
